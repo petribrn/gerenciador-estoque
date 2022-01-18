d717ac55996d2457c66a78c87de56a3245c79ea5
Bruno Petri <brunoopetri@gmail.com>
2022-01-18 19:33:43 -0300

    base user

---
+++ entidade/usuario.py
@@ -0,0 +1,24 @@
+class Usuario:
+    def __init__(self, nome: str, codigo: int):
+        if isinstance(nome, str):
+            self.__nome = nome
+        if isinstance(codigo, int):
+            self.__codigo = codigo
+
+
+    @property
+    def nome(self):
+        return self.__nome
+    @nome.setter
+    def nome(self, nome: str):
+        if isinstance(nome, str):
+            self.__nome = nome
+
+    @property
+    def codigo(self):
+        return self.__codigo
+    @codigo.setter
+    def codigo(self, codigo: int):
+        if isinstance(codigo, int):
+            self.__codigo = codigo
+

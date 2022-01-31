b45e1713796ee66b73334547c06c2248677b6ac1
Bruno Petri <brunoopetri@gmail.com>
2022-01-31 20:16:32 -0300

    product screen/controller

---
+++ controle/controlador_produto.py
@@ -0,0 +1,62 @@
+from limite.tela_produto import TelaProduto
+from entidade.produto import Produto
+class ControladorProduto():
+    def __init__(self, controlador_sistema):
+        self.__produtos = []
+        self.__controlador_sistema = controlador_sistema
+        self.__tela_produto = TelaProduto
+
+    def adiciona_produto(self):
+        dados_produto = self.__tela_produto.pega_dados_produto()
+        produto = Produto(dados_produto["nome"], dados_produto["cor"], dados_produto["tipo"], dados_produto["descricao"], dados_produto["codigo"])
+        self.__produtos.append(produto)
+
+    def exclui_produto(self):
+        self.lista_produto()
+        codigo_produto = self.__tela_produto.seleciona_produto()
+        produto = self.pega_produto_por_codigo(codigo_produto)
+
+        if produto is not None:
+            self.__produtos.remove(produto)
+            self.lista_produto()
+        else:
+            self.__tela_produto.mostra_mensagem(msg="Produto inexistente!")
+
+    def altera_produto(self):
+        self.lista_produto()
+        codigo_produto = self.__tela_produto.seleciona_produto()
+        produto = self.pega_produto_por_codigo(codigo_produto)
+
+        if produto is not None:
+            novos_dados_produto = self.__tela_produto.pega_dados_produto()
+            produto.nome = novos_dados_produto["nome"]
+            produto.cor = novos_dados_produto["cor"]
+            produto.tipo = novos_dados_produto["tipo"]
+            produto.descricao = novos_dados_produto["descricao"]
+            produto.codigo = novos_dados_produto["codigo"]
+            self.lista_produto()
+        else:
+            self.__tela_produto.mostra_mensagem(msg="Produto inexistente!")
+
+    def lista_produto(self):
+        if len(self.__produtos) > 0:
+            for produto in self.__produtos:
+                self.__tela_produto.mostra_produto({"nome": produto.nome, "cor": produto.tipo, "tipo": produto.descricao, "descricao": produto.descricao, "codigo": produto.codigo})
+        else:
+            print("A lista de produtos está vazia.")
+
+    def pega_produto_por_codigo(self, codigo):
+        for produto in self.__produtos:
+            if produto.codigo == codigo:
+                return produto
+        return None
+
+    def retornar(self):
+        self.__controlador_sistema.abre_tela() #Necessário implementação Victor Hugo
+
+    def abre_tela(self):
+        lista_opcoes = {1: self.adiciona_produto, 2: self.exclui_produto, 3: self.lista_produto, 4: self.altera_produto, 0: self.retornar}
+
+        continua = True
+        while continua:
+            lista_opcoes[self.__tela_produto.tela_opcoes()]()
---
+++ limite/tela_produto.py
@@ -0,0 +1,100 @@
+from tela import Tela
+from controle.controlador_produto import ControladorProduto
+
+class TelaProduto(Tela):
+    def __init__(self):
+        self.__controlador = ControladorProduto
+
+    def le_numero_int(self, msg: str = "", inteiros_validos: [] = None):
+        valor = input(msg)
+        try:
+            inteiro = int(valor)
+            if inteiros_validos and inteiro not in inteiros_validos:
+                raise ValueError
+        except ValueError:
+            print("Valor inválido: digite novamente.")
+            if inteiros_validos:
+                print(f"Valores válidos: {inteiros_validos}")
+
+    def tela_opcoes(self):
+        print("-------- PRODUTOS ----------")
+        print("1 - Adicionar produto")
+        print("2 - Excluir produto")
+        print("3 - Listar produtos")
+        print("4 - Alterar produto")
+        print("0 - Retornar")
+
+        opcao = self.le_numero_int("Digite a opcao: ", [1,2,3,4,0])
+        return opcao
+
+    def pega_dados_produto(self):
+        print("-----DADOS DO PRODUTO-----")
+
+        while True:
+            produto = {}
+            try:
+                nome = input("Nome: ")
+                if not isinstance(nome, str):
+                    raise ValueError
+                else:
+                    produto["nome"] = nome
+            except ValueError:
+                print("Digite um nome válido!")
+
+            try:
+                cor = input("Cor: ")
+                if not isinstance(cor, str):
+                    raise ValueError
+                else:
+                    produto["cor"] = cor
+            except ValueError:
+                print("Digite uma cor válida!")
+
+            try:
+                tipo = input("Tipo: ")
+                if not isinstance(tipo, str):
+                    raise ValueError
+                else:
+                    produto["tipo"] = tipo
+            except ValueError:
+                print("Digite um tipo válido!")
+
+            try:
+                descricao = input("Descrição: ")
+                if not isinstance(descricao, str):
+                    raise ValueError
+                else:
+                    produto["descricao"] = descricao
+            except ValueError:
+                print("Digite uma descrição válida!")
+
+            try:
+                codigo = input("Código: ")
+                if not isinstance(codigo, str):
+                    raise ValueError
+                else:
+                    produto["codigo"] = codigo
+            except ValueError:
+                print("Digite um código válido!")
+
+            return produto
+
+    def mostra_produto(self, dados_produto):
+        print(f"NOME DO PRODUTO: {dados_produto['nome']}")
+        print(f"COR: {dados_produto['cor']}")
+        print(f"TIPO: {dados_produto['tipo']}")
+        print(f"DESCRIÇÃO: {dados_produto['descricao']}")
+        print(f"CÓDIGO: {dados_produto['codigo']}")
+        print('\n')
+
+    def seleciona_produto(self):
+        codigo = input("Digite o código do produto: ")
+        return codigo
+
+    def mostra_mensagem(self, msg: str):
+        print(msg)
+
+
+
+
+

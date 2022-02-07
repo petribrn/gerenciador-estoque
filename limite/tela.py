a175dddd32d4dcd2ae3addbe2163f1fcdbeb5ebb
Bruno Petri <brunoopetri@gmail.com>
2022-02-07 10:04:29 -0300

    removed init method

--- limite/tela.py
+++ limite/tela.py
@@ -2,11 +2,6 @@ from abc import ABC, abstractmethod
 
 class Tela(ABC):
 
-    #@abstractmethod
-    #def __init__(self, controlador):
-        #self.__controlador = controlador
-        #pass
-
     @abstractmethod
     def trata_opcoes(self):
         pass

1d2c55525976038ce3e88d76afc2eb0fe3e8c9ba
Bruno Petri <brunoopetri@gmail.com>
2022-02-07 10:27:17 -0300

    input/exception treatments

--- controle/controlador_produto.py
+++ controle/controlador_produto.py
@@ -5,46 +5,73 @@ class ControladorProduto():
     def __init__(self, controlador_sistema):
         self.__produtos = []
         self.__controlador_sistema = controlador_sistema
-        self.__tela_produto = TelaProduto
+        self.__tela_produto = TelaProduto()
 
     def adiciona_produto(self):
-        dados_produto = self.__tela_produto.pega_dados_produto()
+        dados_produto = self.__tela_produto.pega_dados_produto(self.__produtos)
         produto = Produto(dados_produto["nome"], dados_produto["cor"], dados_produto["tipo"], dados_produto["descricao"], dados_produto["codigo"])
         self.__produtos.append(produto)
 
     def exclui_produto(self):
-        self.lista_produto()
-        codigo_produto = self.__tela_produto.seleciona_produto()
-        produto = self.pega_produto_por_codigo(codigo_produto)
+        self.__tela_produto.mostra_mensagem("\n-------- EXCLUIR PRODUTO --------")
+        self.lista_produtos()
 
-        if produto is not None:
-            self.__produtos.remove(produto)
-            self.lista_produto()
+        if len(self.__produtos) < 1:
+            pass
         else:
-            self.__tela_produto.mostra_mensagem(msg="Produto inexistente!")
+            codigo_produto = self.__tela_produto.seleciona_produto()
+            produto = self.pega_produto_por_codigo(codigo_produto)
+
+            if produto is not None:
+                self.__produtos.remove(produto)
+                self.__tela_produto.mostra_mensagem("\nProduto excluido.\n")
+            else:
+                self.__tela_produto.mostra_mensagem("\nProduto inexistente!\n")
 
     def altera_produto(self):
-        self.lista_produto()
-        codigo_produto = self.__tela_produto.seleciona_produto()
-        produto = self.pega_produto_por_codigo(codigo_produto)
+        self.__tela_produto.mostra_mensagem("\n-------- ALTERAR PRODUTO --------")
+        self.lista_produtos()
 
-        if produto is not None:
-            novos_dados_produto = self.__tela_produto.pega_dados_produto()
-            produto.nome = novos_dados_produto["nome"]
-            produto.cor = novos_dados_produto["cor"]
-            produto.tipo = novos_dados_produto["tipo"]
-            produto.descricao = novos_dados_produto["descricao"]
-            produto.codigo = novos_dados_produto["codigo"]
-            self.lista_produto()
+        if len(self.__produtos) < 1:
+            pass
         else:
-            self.__tela_produto.mostra_mensagem(msg="Produto inexistente!")
+            codigo_produto = self.__tela_produto.seleciona_produto()
+            produto = self.pega_produto_por_codigo(codigo_produto)
 
-    def lista_produto(self):
-        if len(self.__produtos) > 0:
+            if produto is not None:
+                novos_dados_produto = self.__tela_produto.pega_dados_produto(self.__produtos)
+                produto.nome = novos_dados_produto["nome"]
+                produto.cor = novos_dados_produto["cor"]
+                produto.tipo = novos_dados_produto["tipo"]
+                produto.descricao = novos_dados_produto["descricao"]
+                produto.codigo = novos_dados_produto["codigo"]
+                self.lista_produtos()
+            else:
+                self.__tela_produto.mostra_mensagem("\nProduto inexistente!\n")
+
+    def lista_um_produto(self):
+        if len(self.__produtos) < 1:
+            self.__tela_produto.mostra_mensagem("\nNao existem produtos cadastrados.")
+            self.__tela_produto.mostra_mensagem("Primeiro cadastre um produto!\n")
+        else:
+            codigo_produto = self.__tela_produto.seleciona_produto()
+            produto = self.pega_produto_por_codigo(codigo_produto)
+
+            if produto is not None:
+                self.__tela_produto.mostra_mensagem("\nProduto encontrado:\n")
+                self.__tela_produto.mostra_produto({"nome": produto.nome,"cor": produto.cor, "tipo": produto.tipo, "descricao": produto.descricao, "codigo": produto.codigo})
+            else:
+                self.__tela_produto.mostra_mensagem("Produto inexistente!\n")
+
+    def lista_produtos(self):
+        quantidade_produtos = len(self.__produtos)
+        if quantidade_produtos < 1:
+            self.__tela_produto.mostra_mensagem("\nNao existem produtos cadastrados.")
+            self.__tela_produto.mostra_mensagem("Primeiro cadastre um produto!\n")
+        else:
+            self.__tela_produto.mostra_mensagem(f"\n{'Existe' if quantidade_produtos < 2 else 'Existem'} {quantidade_produtos} {'produtos cadastrados' if quantidade_produtos > 1 else 'produto cadastrado'}:\n")
             for produto in self.__produtos:
-                self.__tela_produto.mostra_produto({"nome": produto.nome, "cor": produto.tipo, "tipo": produto.descricao, "descricao": produto.descricao, "codigo": produto.codigo})
-        else:
-            print("A lista de produtos está vazia.")
+                self.__tela_produto.mostra_produto({"nome": produto.nome,"cor": produto.cor, "tipo": produto.tipo, "descricao": produto.descricao, "codigo": produto.codigo})
 
     def pega_produto_por_codigo(self, codigo):
         for produto in self.__produtos:
@@ -53,11 +80,14 @@ class ControladorProduto():
         return None
 
     def retornar(self):
-        self.__controlador_sistema.abre_tela() #Necessário implementação Victor Hugo
+        self.__controlador_sistema.abre_tela()
 
     def abre_tela(self):
-        lista_opcoes = {1: self.adiciona_produto, 2: self.exclui_produto, 3: self.lista_produto, 4: self.altera_produto, 0: self.retornar}
+        lista_opcoes = {1: self.adiciona_produto, 2: self.altera_produto, 3: self.lista_um_produto, 4: self.lista_produtos,5: self.exclui_produto, 0: self.retornar}
 
         continua = True
         while continua:
-            lista_opcoes[self.__tela_produto.tela_opcoes()]()
+            try:
+                lista_opcoes[self.__tela_produto.tela_opcoes()]()
+            except Exception:
+                pass

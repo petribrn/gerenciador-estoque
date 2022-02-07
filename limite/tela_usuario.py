b7d6a5952e89592b99a479af9b869b34ba1360a3
Bruno Petri <brunoopetri@gmail.com>
2022-02-06 23:15:32 -0300

    input/exception treatments

--- limite/tela_usuario.py
+++ limite/tela_usuario.py
@@ -1,9 +1,6 @@
 from limite.tela import Tela
-from controle.controlador_usuario import ControladorUsuario
 
 class TelaUsuario(Tela):
-    def __init__(self):
-        self.__controlador = ControladorUsuario
 
     def trata_opcoes(self, msg: str = "", inteiros_validos: [] = None):
         valor = input(msg)
@@ -11,13 +8,14 @@ class TelaUsuario(Tela):
             inteiro = int(valor)
             if inteiros_validos and inteiro not in inteiros_validos:
                 raise ValueError
+            return inteiro
         except ValueError:
-            print("Valor inválido: digite novamente.")
+            print("\nValor inválido: digite novamente.")
             if inteiros_validos:
                 print(f"Valores válidos: {inteiros_validos}")
 
     def tela_opcoes(self):
-        print("-------- PRODUTOS ----------")
+        print("\n-------- USUARIOS --------")
         print("1 - Incluir usuario")
         print("2 - Alterar usuario")
         print("3 - Listar um usuario")
@@ -25,42 +23,57 @@ class TelaUsuario(Tela):
         print("5 - Excluir usuario")
         print("0 - Retornar")
 
-        opcao = self.trata_opcoes("Digite a opcao: ", [1,2,3,4,0])
+        opcao = self.trata_opcoes("\nDigite a opcao: ", [1,2,3,4,5,0])
         return opcao
 
-    def pega_dados_usuario(self):
-        print("-----DADOS DO USUARIO-----")
+    def pega_dados_usuario(self, lista_de_usuarios):
+        print("\n-----DADOS DO USUARIO-----")
+        usuario = {}
+        codigos = []
+
+        for user in lista_de_usuarios:
+            codigos.append(user.codigo)
 
         while True:
-            usuario = {}
+            nome = input("Nome: ")
             try:
-                nome = input("Nome: ")
-                if not isinstance(nome, str):
-                    raise ValueError
-                else:
-                    usuario["nome"] = nome
+                int(nome)
+                print("\nDigite um nome valido!")
             except ValueError:
-                print("Digite um nome válido!")
-
+                usuario["nome"] = nome
+                break
+        while True:
+            codigo = input("Codigo: ")
             try:
-                codigo = input("Código: ")
-                if not isinstance(codigo, str):
+                if codigo.isnumeric() == False:
                     raise ValueError
-                else:
-                    usuario["codigo"] = codigo
+                if int(codigo) in codigos:
+                    raise Exception
+                usuario["codigo"] = int(codigo)
+                break
             except ValueError:
-                print("Digite um código válido!")
+                print("\nDigite um valor valido!")
+            except Exception:
+                print("\nO codigo ja esta cadastrado!")
 
-            return usuario
+        return usuario
 
     def mostra_usuario(self, dados_usuario):
+        print("-"*18 + ("-"*len(dados_usuario["nome"])))
         print(f"NOME DO USUARIO: {dados_usuario['nome']}")
-        print(f"CÓDIGO: {dados_usuario['codigo']}")
-        print('\n')
+        print(f"CODIGO: {dados_usuario['codigo']}")
+        print("-" * 18 + ("-" * len(dados_usuario["nome"]))+"\n")
+
 
     def seleciona_usuario(self):
-        codigo = input("Digite o código do usuario: ")
-        return codigo
+        while True:
+            codigo = input("\nDigite o codigo do usuario: ")
+            try:
+                if codigo.isnumeric() == False:
+                    raise ValueError
+                return int(codigo)
+            except ValueError:
+                print("Digite um valor valido!")
 
     def mostra_mensagem(self, msg: str):
         print(msg)
